5804fc62bb9b23d7e90e33da9bbea278361d1949
1552980358 <1552980358@qq.com>
2023-11-11 00:30:50 +0800

    Bump to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.chan0821.ban.shake.ad"
         minSdk = 21
         targetSdk = compileSdk
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
